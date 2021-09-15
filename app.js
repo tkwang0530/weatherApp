6bd6fbaa855e971bcaee72332edc77a63647fea4
Tommy <tkwang0530@gmail.com>
2021-09-15 23:27:17 +0800

    add custom error handler

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const app = require('express')();
+const { handleError } = require('./helpers/error');
 const indexRouter = require('./routes/index')
 const weatherRecordRouter = require('./routes/weather_record')
 
 // Connect all our routes to our application
 app.use('/', indexRouter);
 app.use('/weather_record', weatherRecordRouter);
+app.use((err, req, res, next) => {
+    handleError(err, res);
+})
 
 app.listen(8080, () => {
     console.log('Application listening on port 8080!');
